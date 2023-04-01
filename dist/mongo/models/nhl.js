84ae11366e3bd8d591e53f111590a2c0824fcc43
Santi <santi.cocciardi@gmail.com>
2023-03-31 21:53:32 -0300

    start npm

---
+++ dist/mongo/models/nhl.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const nhlSchema = new mongoose_1.Schema({
+    equipoLocal: { type: String },
+    equipoVisitante: { type: String },
+    puntosLocal: { type: Number },
+    puntosVisitante: { type: Number },
+    hora: { type: String },
+    date: Date,
+    apuestas: {
+        handicapLocal: [
+            {
+                type: String,
+                value: Number,
+            },
+        ],
+        handicapVisitante: [
+            {
+                type: String,
+                value: Number,
+            },
+        ],
+        puntosAlta: [
+            {
+                type: String,
+                value: Number,
+            },
+        ],
+        puntosBaja: [
+            {
+                type: String,
+                value: Number,
+            },
+        ],
+        ganadorLocal: { type: Number },
+        ganadorVisitante: { type: Number },
+    }
+});
+exports.default = (0, mongoose_1.model)('Nhl', nhlSchema);
